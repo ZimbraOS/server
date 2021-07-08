20e74261759e7dd2ddaef3fc5e48f83ad5981a4a
sreejan <sreejan88@gmail.com>
2021-07-08 20:31:39 +0530

    adding jq binary, onlyoffice config script, https servers, create onlyoffice db

--- DocService/sources/server.js
+++ DocService/sources/server.js
@@ -42,6 +42,7 @@ const fs = require('fs');
 
 const express = require('express');
 const http = require('http');
+const https = require('https');
 const urlModule = require('url');
 const path = require('path');
 const bodyParser = require("body-parser");
@@ -63,12 +64,23 @@ const cfgHtmlTemplate = configCommon.get('wopi.htmlTemplate');
 const cfgTokenEnableBrowser = configCommon.get('services.CoAuthoring.token.enable.browser');
 const cfgTokenEnableRequestInbox = configCommon.get('services.CoAuthoring.token.enable.request.inbox');
 const cfgTokenEnableRequestOutbox = configCommon.get('services.CoAuthoring.token.enable.request.outbox');
+const cfgLicenseFile = configCommon.get('license.license_file');
+const cfgDownloadMaxBytes = configCommon.get('FileConverter.converter.maxDownloadBytes');
 
 const app = express();
-//path.resolve uses __dirname by default(unexpected path in pkg)
-app.set("views", path.resolve(process.cwd(), cfgHtmlTemplate));
-app.set("view engine", "ejs");
-const server = http.createServer(app);
+
+let server = null;
+
+if (config.has('ssl')) {
+	const privateKey = fs.readFileSync(config.get('ssl.key')).toString();
+	const certificateKey = fs.readFileSync(config.get('ssl.cert')).toString();
+	//See detailed options format here: http://nodejs.org/api/tls.html#tls_tls_createserver_options_secureconnectionlistener
+	const options = {key: privateKey, cert: certificateKey};
+
+	server = https.createServer(options, app);
+} else {
+	server = http.createServer(app);
+}
 
 let licenseInfo, licenseOriginal, updatePluginsTime, userPlugins, pluginsLoaded;
 
