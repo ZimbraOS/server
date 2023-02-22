363bc97aab72a6ca41a439c897357999801b52ce
UmaShankar Avagadda <umagmrit@gmail.com>
2023-02-22 03:43:17 +0000

    added circleci support

---
+++ scripts/postinst.sh
@@ -0,0 +1,16 @@
+if [ -f /opt/zimbra/onlyoffice/bin/jq ]; then
+	chmod +x /opt/zimbra/onlyoffice/bin/jq
+fi
+
+if [ -f /opt/zimbra/onlyoffice/bin/zmonlyofficeconfig ]; then
+	chmod +x /opt/zimbra/onlyoffice/bin/zmonlyofficeconfig
+fi
+
+if [ -f /opt/zimbra/onlyoffice/bin/process_id.json ]; then
+	chmod 775 /opt/zimbra/onlyoffice/bin/process_id.json
+	chown zimbra:zimbra /opt/zimbra/onlyoffice/bin/process_id.json
+fi
+
+if [ -d /opt/zimbra/onlyoffice/documentserver ]; then
+	chown -R zimbra:zimbra /opt/zimbra/onlyoffice/documentserver/
+fi
